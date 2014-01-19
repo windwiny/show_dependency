4520ebd4df8e19de4bea2463cc496ac17a18cb61
windwiny <windwiny.ubt@gmail.com>
2014-01-19 20:36:03 +0800

    ffi module inherit bug

--- bin/show_ffi_inherit.rb
+++ bin/show_ffi_inherit.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby -w
 
 require "ffi"
+require "set"
 
 unless (ARGV & ['-h', '--help']).empty?
   abort <<-EOS
@@ -21,15 +22,20 @@ cls = FFI.constants.uniq.select{ |x| x !~ /Error/i }
 
 cls.sort_by! { |x| [x.ancestors.size, x.ancestors[1..-1].to_s] }
 
-gr_edges = []
-cls.each do |m|
-    v = m.ancestors
-    v.each_cons(2) do |x, y|
+gr_edges = Set.new
+gr_mods = Set.new
+cls.each do |cl1|
+    ances = cl1.ancestors
+    ances.select { |x| x.class == Class }.each_cons(2) do |x, y|
       gr_edges << "  #{format '%-22s', x.name.split('::')[-1]}  ->  #{y.name.split('::')[-1]}"
+    end
+    ances[1..-1].each do |m|
+      break if m.class == Class
+      gr_edges << "  #{format '%-22s', cl1.name.split('::')[-1]}  ->  #{m.name.split('::')[-1]}  [color=green]"
+      gr_mods << "  #{m.name.split('::')[-1]} [color=green]"
     end
 end
 
-gr_edges.uniq!
 gr_most_use = %w{MemoryPointer Struct Union Enums Buffer }.map { |x| "  #{x}  [color=blue]" }
 #---------
 
@@ -42,7 +48,9 @@ else
   o = $stdout
 end
 o.puts "digraph G {"
-o.puts gr_edges
+o.puts gr_edges.to_a.sort
+o.puts
+o.puts gr_mods.to_a.sort
 o.puts
 o.puts gr_most_use
 o.puts
